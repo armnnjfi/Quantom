6f1b90bd44b7daa5dce1bc3001c09bdb835c2f41
h434ni <h434ni@proton.me>
2024-12-11 22:55:02 +0330

    added dependencies for adding compose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,13 @@ plugins {
 android {
     namespace = "com.example.quantom"
     compileSdk = 34
+    buildFeatures {
+        compose = true
+    }
 
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.4"
+    }
     defaultConfig {
         applicationId = "com.example.quantom"
         minSdk = 24
@@ -36,7 +42,16 @@ android {
 }
 
 dependencies {
-
+//This dependency integrates Jetpack Compose with the Activity class, enabling the use of Compose in Android activities.
+    implementation(libs.androidx.activity.compose)
+//Compose Bill of Materials (BOM), which helps manage versions of Compose libraries. In this case, it's using version 2023.03.00 for Compose dependencies.
+    implementation(platform(libs.androidx.compose.bom))
+//This dependency includes the fundamental UI elements and features provided by Jetpack Compose.
+    implementation(libs.androidx.ui)
+//This includes tooling and preview functionalities for Compose, assisting with development and debugging UI components.
+    implementation(libs.androidx.ui.tooling.preview)
+//This dependency includes the Material Design 3 components and styles adapted for Jetpack Compose, allowing the implementation of Material Design principles in your app's UI
+    implementation(libs.androidx.material3)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
