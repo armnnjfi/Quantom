a5728b121a23ceb036c3f8cc7bdb8e86cca350a5
faridbahri <farid.bh1234@gmail.com>
2024-12-11 23:54:34 +0330

    ss

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.quantom"
-    compileSdk = 34
+    compileSdk = 35
     buildFeatures {
         compose = true
     }
--- app/src/main/java/com/example/quantom/MainActivity.kt
+++ app/src/main/java/com/example/quantom/MainActivity.kt
@@ -5,12 +5,60 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.example.quantom.fragments.CategoryFragment
+import com.example.quantom.fragments.HomeFragment
+import com.example.quantom.fragments.ProfileFragment
+import com.example.quantom.fragments.TaskFragment
+import com.google.android.material.bottomappbar.BottomAppBar
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    val bottomAppBar: BottomAppBar = findViewById(R.id.bottom_app_bar)
+    val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottomNavigationView)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        setSupportActionBar(bottomAppBar)
+
+
+
+
+        bottomNavigationView.setOnNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.action_home -> {
+                    replaceFragment(HomeFragment())
+                    true
+                }
+
+                R.id.action_search -> {
+                    replaceFragment(CategoryFragment())
+                    true
+                }
+
+                R.id.action_notifications -> {
+                    replaceFragment(TaskFragment())
+                    true
+                }
+
+                R.id.action_profile -> {
+                    replaceFragment(ProfileFragment())
+                    true
+                }
+
+                else -> false
+            }
+        }
 
     }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.content_frame, fragment)
+            .commit()
+    }
+
+
+
 }
