d1bdb84fd91fb3af91391917765a2e692fe300b3
faridbahri <farid.bh1234@gmail.com>
2024-12-12 13:20:45 +0330

    bug fixed

--- app/src/main/res/layout/myproject/MainActivity.kt
+++ app/src/main/res/layout/myproject/MainActivity.kt
@@ -3,7 +3,8 @@ package com.example.myproject
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import com.example.quantom.R
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/res/layout/myproject/fragments/HomeFragment.kt
+++ app/src/main/res/layout/myproject/fragments/HomeFragment.kt
@@ -2,6 +2,10 @@ package com.example.myproject.fragments
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.myproject.R
 
 class HomeFragment : Fragment() {
 
