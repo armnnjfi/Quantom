299f622dcab94ebc55c3a3d6e7e63a265273f9c2
faridbahri <farid.bh1234@gmail.com>
2024-12-12 16:07:07 +0330

    bug fixed2

--- app/src/main/java/com/example/quantom/MainActivity.kt
+++ app/src/main/java/com/example/quantom/MainActivity.kt
@@ -3,13 +3,50 @@ package com.example.quantom
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import com.example.quantom.R
+import com.example.quantom.fragments.HomeFragment
+import com.example.quantom.fragments.ProfileFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bottomNavigation: BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        init()
 
     }
+
+
+    private fun bindViews() {
+        bottomNavigation = findViewById(R.id.bottomNavigationView)
+
+    }
+
+    fun init() {
+        bindViews()
+        bottomNavigation.setOnNavigationItemSelectedListener() { item ->
+            when (item.itemId) {
+                R.id.action_home -> {
+                    var transaction = supportFragmentManager.beginTransaction()
+                    transaction.replace(R.id.content_frame, HomeFragment())
+                    transaction.commit()
+                    true
+                }
+
+                R.id.action_profile -> {
+                    var transaction = supportFragmentManager.beginTransaction()
+                    transaction.replace(R.id.content_frame, ProfileFragment())
+                    transaction.commit()
+                    true
+                }
+
+                else -> false
+            }
+        }
+        bottomNavigation.selectedItemId = R.id.action_home
+
+    }
+
+
 }
