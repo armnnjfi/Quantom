8413450a7a83d24ec026cc4a62b82c3deacf6b62
faridbahri <farid.bh1234@gmail.com>
2024-12-12 14:20:08 +0330

    bug fixed2

---
+++ app/src/main/java/com/example/quantom/fragments/HomeFragment.kt
@@ -0,0 +1,17 @@
+package com.example.myproject.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+
+class HomeFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+
+        }
+    }
+
+
+}
