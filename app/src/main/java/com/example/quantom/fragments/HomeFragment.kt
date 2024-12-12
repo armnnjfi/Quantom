333a06824a27da8e2224f3ac0a156b383131fdd7
h434ni <h434ni@proton.me>
2024-12-12 19:54:22 +0330

    silder

--- app/src/main/java/com/example/quantom/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/quantom/fragments/HomeFragment.kt
@@ -5,10 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.foundation.layout.Column
 import androidx.compose.ui.platform.ComposeView
 import com.example.quantom.R
-import com.example.quantom.composes.GlideImageScreen
 import com.example.quantom.composes.TopSlider
 
 // TODO: Rename parameter arguments, choose names that match
