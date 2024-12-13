01b63bfc9ce84d079812c496844354a4d5b1f2c6
h434ni <h434ni@proton.me>
2024-12-13 06:43:06 +0330

    make task page

--- app/src/main/java/com/example/quantom/fragments/TaskFragment.kt
+++ app/src/main/java/com/example/quantom/fragments/TaskFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.ui.platform.ComposeView
 import com.example.quantom.R
+import com.example.quantom.composes.Javayez
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -34,8 +36,12 @@ class TaskFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_task, container, false)
+        val view = inflater.inflate(R.layout.fragment_task, container, false)
+        val composeView = view.findViewById<ComposeView>(R.id.composeViewJavayez)
+        composeView.setContent {
+            Javayez()
+        }
+        return view
     }
 
     companion object {
